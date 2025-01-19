3742cb33154b035e6a0f8b078366cde80052f017
SaiKiranMatta <saikirangoud.matta@gmail.com>
2025-01-20 01:29:55 +0530

    update contract to work with webauthn

--- smart-account-contracts/src/SmartAccount.sol
+++ smart-account-contracts/src/SmartAccount.sol
@@ -7,22 +7,44 @@ import "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@account-abstraction/contracts/interfaces/IAccount.sol";
 import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
-import "@openzeppelin/contracts/utils/Create2.sol";
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "lib/webauthn-sol/src/WebAuthn.sol";
 
-contract SmartAccount is IAccount, UUPSUpgradeable, Ownable {
+contract SmartAccount is IAccount, UUPSUpgradeable, Initializable {
+    using ECDSA for bytes32;
+    
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
     error SmartAccount__NotFromEntryPoint();
     error SmartAccount__NotFromEntryPointOrOwner();
     error SmartAccount__CallFailed(bytes);
+    error SmartAccount__AlreadyInitialized();
+    error SmartAccount__InvalidSignatureLength();
+    error SmartAccount__InvalidOwner();
+    error SmartAccount__InvalidSignature();
 
     /*//////////////////////////////////////////////////////////////
                             STATE VARIABLES
     //////////////////////////////////////////////////////////////*/
     IEntryPoint private immutable i_entryPoint;
+
+    // Owner's WebAuthn public key components
+    uint256 public ownerPubKeyX;
+    uint256 public ownerPubKeyY;
+
+    // Session keys with validity
+    struct SessionKeyData {
+        uint256 validUntil;
+        bool isValid;
+    }
+
+    mapping(address => SessionKeyData) public sessionKeys;
+    uint256 public constant SESSION_VALIDITY = 1 days;
+
+    event SessionKeyCreated(address indexed key, uint256 validUntil);
+    event Initialized(uint256 pubKeyX, uint256 pubKeyY);
 
     /*//////////////////////////////////////////////////////////////
                                MODIFIERS
@@ -35,7 +57,7 @@ contract SmartAccount is IAccount, UUPSUpgradeable, Ownable {
     }
 
     modifier requireFromEntryPointOrOwner() {
-        if (msg.sender != address(i_entryPoint) && msg.sender != owner()) {
+        if (msg.sender != address(i_entryPoint) && !_isOwner(msg.sender)) {
             revert SmartAccount__NotFromEntryPointOrOwner();
         }
         _;
@@ -44,74 +66,112 @@ contract SmartAccount is IAccount, UUPSUpgradeable, Ownable {
     /*//////////////////////////////////////////////////////////////
                                FUNCTIONS
     //////////////////////////////////////////////////////////////*/
-    constructor(address entryPoint) Ownable(msg.sender) {
+    constructor(address entryPoint) {
         i_entryPoint = IEntryPoint(entryPoint);
+        _disableInitializers();
+    }
+
+    function initialize(
+        uint256 _ownerPubKeyX,
+        uint256 _ownerPubKeyY
+    ) public initializer {
+        ownerPubKeyX = _ownerPubKeyX;
+        ownerPubKeyY = _ownerPubKeyY;
+        emit Initialized(_ownerPubKeyX, _ownerPubKeyY);
     }
 
     receive() external payable {}
 
     /* //////////////////////////////////////////////////////////////
-    DECLERATIONS 
+                           EXTERNAL FUNCTIONS
     ////////////////////////////////////////////////////////////// */
 
-    using ECDSA for bytes32;
-
-    mapping(address => uint256) public sessionKeys;
-    uint256 public constant SESSION_VALIDITY = 1 days;
-
-    event SessionKeyCreated(address indexed key, uint256 validUntil);
-
-
-    /*//////////////////////////////////////////////////////////////
-                           EXTERNAL FUNCTIONS
-    //////////////////////////////////////////////////////////////*/
-    function execute(address dest, uint256 value, bytes calldata functionData) external requireFromEntryPointOrOwner {
-        (bool success, bytes memory result) = dest.call{value: value}(functionData);
+    function execute(
+        address dest,
+        uint256 value,
+        bytes calldata functionData
+    ) external requireFromEntryPointOrOwner {
+        (bool success, bytes memory result) = dest.call{value: value}(
+            functionData
+        );
         if (!success) {
             revert SmartAccount__CallFailed(result);
         }
     }
 
-    // A signature is valid, if it's the SmartAccount owner
-    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
-        external
-        requireFromEntryPoint
-        returns (uint256 validationData)
-    {
+    function createSessionKey(address sessionKeyAddress, bytes calldata webAuthnData) external requireFromEntryPointOrOwner {
+        WebAuthn.WebAuthnAuth memory auth = abi.decode(webAuthnData, (WebAuthn.WebAuthnAuth));
+        
+        bool isValid = WebAuthn.verify({
+            challenge: abi.encode(keccak256(abi.encodePacked(sessionKeyAddress))),
+            requireUV: true,
+            webAuthnAuth: auth,
+            x: ownerPubKeyX,
+            y: ownerPubKeyY
+        });
+        
+        if (!isValid) {
+            revert SmartAccount__InvalidOwner();
+        }
+        
+        sessionKeys[sessionKeyAddress] = SessionKeyData({
+            validUntil: block.timestamp + SESSION_VALIDITY,
+            isValid: true
+        });
+
+        emit SessionKeyCreated(sessionKeyAddress, sessionKeys[sessionKeyAddress].validUntil);
+    }
+
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external requireFromEntryPoint returns (uint256 validationData) {
         validationData = _validateSignature(userOp, userOpHash);
-        // _validateNonce()
         _payPrefund(missingAccountFunds);
     }
 
-    function createSessionKey(address sessionKey) external {
-        require(msg.sender == owner(), "Only owner can create session keys");
-        sessionKeys[sessionKey] = block.timestamp + SESSION_VALIDITY;
-        emit SessionKeyCreated(sessionKey, sessionKeys[sessionKey]);
-    }
-
-
-
-   /*//////////////////////////////////////////////////////////////
+    /*//////////////////////////////////////////////////////////////
                            INTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
     function _validateSignature(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash
-    ) internal virtual returns (uint256 validationData) {
-        bytes32 hash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
-
-        // Check if it's a session key
-        address recovered = hash.recover(userOp.signature);
-        if (sessionKeys[recovered] > block.timestamp) {
-            return 0; // Valid session key
+    ) internal view returns (uint256 validationData) {
+        (bool isSessionKey, bytes memory signature) = abi.decode(userOp.signature, (bool, bytes));
+        
+        if (isSessionKey) {
+            // For session keys, recover address from ECDSA signature and verify validity
+            bytes32 hash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+            address recoveredAddress = hash.recover(signature);
+            
+            SessionKeyData memory sessionData = sessionKeys[recoveredAddress];
+            if (sessionData.isValid && sessionData.validUntil > block.timestamp) {
+                return 0;
+            }
+            return SIG_VALIDATION_FAILED;
+        } else {
+            // Owner signature validation using WebAuthn
+            WebAuthn.WebAuthnAuth memory auth = abi.decode(signature, (WebAuthn.WebAuthnAuth));
+            bool isValidOwner = WebAuthn.verify({
+                challenge: abi.encode(userOpHash),
+                requireUV: true,
+                webAuthnAuth: auth,
+                x: ownerPubKeyX,
+                y: ownerPubKeyY
+            });
+            return isValidOwner ? 0 : SIG_VALIDATION_FAILED;
         }
+    }
 
-        // Validate passkey signature
-        if (recovered == owner()) {
-            return 0;
-        }
-
-        return SIG_VALIDATION_FAILED;
+    function _isOwner(address caller) internal view returns (bool) {
+        // Create deterministic address from owner's public key
+        address ownerAddress = address(
+            uint160(
+                uint256(keccak256(abi.encodePacked(ownerPubKeyX, ownerPubKeyY)))
+            )
+        );
+        return caller == ownerAddress;
     }
 
     function _payPrefund(uint256 missingAccountFunds) internal {
@@ -124,12 +184,11 @@ contract SmartAccount is IAccount, UUPSUpgradeable, Ownable {
         }
     }
 
-    function _authorizeUpgrade(
-        address /*newImplementation*/
-    ) internal view override {
-        require(msg.sender == owner(), "Only owner can upgrade");
+    function _authorizeUpgrade(address) internal view override {
+        if (!_isOwner(msg.sender)) {
+            revert SmartAccount__NotFromEntryPointOrOwner();
+        }
     }
-
 
     /*//////////////////////////////////////////////////////////////
                                 GETTERS
